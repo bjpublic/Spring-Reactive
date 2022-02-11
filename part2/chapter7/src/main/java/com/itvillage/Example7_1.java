eda7a0159c52ae42d6511998fc5a697f6703c19f
hjs6877 <hjs6877@example.com>
2022-02-11 19:20:32 +0900

    Add Cold and Hot Sequence Examples

--- part2/chapter7/src/main/java/com/itvillage/Example7_1.java
+++ part2/chapter7/src/main/java/com/itvillage/Example7_1.java
@@ -1,19 +1,25 @@
 package com.itvillage;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
 import java.util.Arrays;
 
+/**
+ * Cold Sequence 예제
+ */
+@Slf4j
 public class Example7_1 {
     public static void main(String[] args) throws InterruptedException {
+
         Flux<String> coldFlux =
                 Flux
                     .fromIterable(Arrays.asList("KOREA", "JAPAN", "CHINESE"))
                     .map(String::toLowerCase);
 
-        coldFlux.subscribe(country -> System.out.println("# Subscriber1: " + country));
+        coldFlux.subscribe(country -> log.info("# Subscriber1: {}", country));
         System.out.println("-------------------------");
         Thread.sleep(2000L);
-        coldFlux.subscribe(country -> System.out.println("# Subscriber2: " + country));
+        coldFlux.subscribe(country -> log.info("# Subscriber2: {}", country));
     }
 }
