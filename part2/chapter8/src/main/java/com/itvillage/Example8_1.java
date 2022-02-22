e420a72723d9f06306b557a35bd017be121d7b72
hjs6877 <hjs6877@gmail.com>
2022-02-22 23:21:38 +0900

    Implementing Chapter 8 Examples

---
+++ part2/chapter8/src/main/java/com/itvillage/Example8_1.java
@@ -0,0 +1,32 @@
+package com.itvillage;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+
+/**
+ * Subscriber가 처리 가능한 만큼의 request 개수를 조절하는 Backpressure 예제
+ */
+@Slf4j
+public class Example8_1 {
+    public static void main(String[] args) {
+        Flux.range(1, 5)
+            .doOnRequest(data -> log.info("# doOnRequest: {}", data))
+            .subscribe(new BaseSubscriber<Integer>() {
+                @Override
+                protected void hookOnSubscribe(Subscription subscription) {
+                    request(1);
+                }
+
+                @SneakyThrows
+                @Override
+                protected void hookOnNext(Integer value) {
+                    Thread.sleep(2000L);
+                    log.info("# hookOnNext: {}", value);
+                    request(1);
+                }
+            });
+    }
+}
