465d49d763158d1058926afba58c9d0261144c26
hjs6877 <hjs6877@gmail.com>
2022-03-13 20:46:48 +0900

    Add Example10_2, 3, 4

--- part2/src/main/java/chapter10/Example10_2.java
+++ part2/src/main/java/chapter10/Example10_2.java
@@ -12,8 +12,9 @@ import reactor.core.scheduler.Schedulers;
 public class Example10_2 {
     public static void main(String[] args) throws InterruptedException {
         Flux.fromArray(new Integer[] {1, 3, 5, 7})
-                .subscribeOn(Schedulers.boundedElastic())
+                .doOnNext(data -> log.info("# doOnNext: {}", data))
                 .doOnSubscribe(subscription -> log.info("# doOnSubscribe"))
+                .publishOn(Schedulers.parallel())
                 .subscribe(data -> log.info("# onNext: {}", data));
 
         Thread.sleep(500L);
---
+++ part2/src/main/java/chapter10/Example10_4.java
@@ -0,0 +1,23 @@
+package chapter10;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+/**
+ * parallel() 기본 사용 예제
+ * - parallel()만 사용할 경우에는 병렬로 작업을 수행하지 않는다.
+ * - runOn()을 사용해서 Scheduler를 할당해주어야 병렬로 작업을 수행한다.
+ * - **** CPU 코어 갯수내에서 worker thread를 할당한다. ****
+ */
+@Slf4j
+public class Example10_4 {
+    public static void main(String[] args) throws InterruptedException {
+        Flux.fromArray(new Integer[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19})
+                .parallel(4)
+                .runOn(Schedulers.parallel())
+                .subscribe(data -> log.info("# onNext: {}", data));
+
+        Thread.sleep(100L);
+    }
+}
