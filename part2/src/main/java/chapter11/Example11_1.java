7b238271020de508415870b9f43971796256d45b
hjs6877 <hjs6877@gmail.com>
2022-03-19 22:11:25 +0900

    Add Example11_1, 11_3

--- part2/src/main/java/chapter11/Example11_1.java
+++ part2/src/main/java/chapter11/Example11_1.java
@@ -6,10 +6,11 @@ import reactor.core.scheduler.Schedulers;
 
 /**
  * Context 기본 예제
- *  -
- *  -
- *  -
- *  -
+ *  - contextWrite() Operator로 Context에 데이터 쓰기 작업을 할 수 있다.
+ *  - Context.put()으로 Context에 데이터를 쓸 수 있다.
+ *  - deferContextual() Operator로 Context에 데이터 읽기 작업을 할 수 있다.
+ *  - Context.get()으로 Context에서 데이터를 읽을 수 있다.
+ *  - transformDeferredContextual() Operator로 Operator 중간에서 Context에 데이터 읽기 작업을 할 수 있다.
  */
 @Slf4j
 public class Example11_1 {
---
+++ part2/src/main/java/chapter11/Example11_3.java
@@ -0,0 +1,31 @@
+package chapter11;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.util.context.Context;
+
+/**
+ * Context API 사용 예제
+ */
+@Slf4j
+public class Example11_3 {
+    public static void main(String[] args) throws InterruptedException {
+        final String key1 = "company";
+        final String key2 = "firstName";
+        final String key3 = "lastName";
+
+        Mono
+            .deferContextual(ctx ->
+                    Mono.just(ctx.get(key1) + ", " + ctx.get(key2) + " " + ctx.get(key3))
+            )
+            .publishOn(Schedulers.parallel())
+            .contextWrite(context ->
+                    context.putAll(Context.of(key2, "Steve", key3, "Jobs").readOnly())
+            )
+            .contextWrite(context -> context.put(key1, "Apple"))
+            .subscribe(data -> log.info("# onNext: {}" , data));
+
+        Thread.sleep(100L);
+    }
+}
