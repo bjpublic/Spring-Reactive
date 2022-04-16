a9c582f5190611b61f771c49a13799cb1eaf1c58
hjs6877 <hjs6877@gmail.com>
2022-04-16 21:53:49 +0900

    Add Example14_1

---
+++ part2/src/main/java/part14/Example14_1.java
@@ -0,0 +1,15 @@
+package part14;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+public class Example14_1 {
+    public static void main(String[] args) {
+        Mono
+            .justOrEmpty(null)
+            .subscribe(data -> {},
+                    error -> {},
+                    () -> log.info("# onComplete"));
+    }
+}
