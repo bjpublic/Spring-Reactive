ce702cdf5cf2a578e5c1d782e8450918b732b0c2
hjs6877 <hjs6877@gmail.com>
2022-05-03 00:01:42 +0900

    Complete Creating Operators

---
+++ part2/src/main/java/chapter14/Example14_14.java
@@ -0,0 +1,40 @@
+package chapter14;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.scheduler.Schedulers;
+
+/**
+ * create 예제
+ *  - Backpressure 전략 적용
+ */
+@Slf4j
+public class Example14_14 {
+    public static int start = 1;
+    public static int end = 4;
+    public static void main(String[] args) throws InterruptedException {
+        Flux.create((FluxSink<Integer> emitter) -> {
+            emitter.onRequest(n -> {
+                log.info("# requested: " + n);
+                try {
+                    Thread.sleep(500L);
+                    for (int i = start; i <= end; i++) {
+                        emitter.next(i);
+                    }
+                    start += 4;
+                    end += 4;
+                } catch (InterruptedException e) {}
+            });
+
+            emitter.onDispose(() -> {
+                log.info("# clean up");
+            });
+        }, FluxSink.OverflowStrategy.DROP)
+        .subscribeOn(Schedulers.boundedElastic())
+        .publishOn(Schedulers.parallel(), 2)
+        .subscribe(data -> log.info("# onNext: {}", data));
+
+        Thread.sleep(3000L);
+    }
+}
