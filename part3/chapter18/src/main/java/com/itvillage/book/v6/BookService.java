d5e80dd004f054809f3e793f9ea01733c38c4459
hjs6877 <hjs6877@gmail.com>
2022-07-24 23:46:41 +0900

    Complete BookService for R2dbcEntityTemplate

--- part3/chapter18/src/main/java/com/itvillage/book/v6/BookService.java
+++ part3/chapter18/src/main/java/com/itvillage/book/v6/BookService.java
@@ -1,28 +1,36 @@
 package com.itvillage.book.v6;
 
+import com.itvillage.book.exception.BusinessLogicException;
+import com.itvillage.book.exception.ExceptionCode;
 import com.itvillage.utils.CustomBeanUtils;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
+import static org.springframework.data.relational.core.query.Criteria.where;
+import static org.springframework.data.relational.core.query.Query.query;
+
 @Slf4j
 @Service("bookServiceV6")
 @RequiredArgsConstructor
 public class BookService {
+    private final @NonNull R2dbcEntityTemplate template;
     private final @NonNull CustomBeanUtils<Book> beanUtils;
 
     public Mono<Book> saveBook(Book book) {
-        // TODO
-        return null;
+        return verifyExistIsbn(book.getIsbn())
+                .then(template.insert(book));
     }
 
     public Mono<Book> updateBook(Book book) {
-        // TODO
-        return null;
+        return findVerifiedBook(book.getBookId())
+                .map(findBook -> beanUtils.copyNonNullProperties(book, findBook))
+                .flatMap(updatingBook -> template.update(updatingBook));
     }
 
     public Mono<Book> findBook(long bookId) {
@@ -30,17 +38,24 @@ public class BookService {
     }
 
     public Mono<List<Book>> findBooks() {
-        // TODO
-        return null;
+        return template.select(Book.class).all().collectList();
     }
 
     private Mono<Void> verifyExistIsbn(String isbn) {
-        // TODO
-        return null;
+        return template.selectOne(query(where("ISBN").is(isbn)), Book.class)
+                .flatMap(findBook -> {
+                    if (findBook != null) {
+                        return Mono.error(new BusinessLogicException(
+                                ExceptionCode.BOOK_EXISTS));
+                    }
+                    return Mono.empty();
+                });
     }
 
     private Mono<Book> findVerifiedBook(long bookId) {
-        // TODO
-        return null;
+        return template.selectOne(query(where("BOOK_ID").is(bookId))
+                                                                        , Book.class)
+                .switchIfEmpty(Mono.error(new BusinessLogicException(
+                                                ExceptionCode.BOOK_NOT_FOUND)));
     }
 }
