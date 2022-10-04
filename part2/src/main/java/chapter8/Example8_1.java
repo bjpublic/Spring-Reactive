d92b063d9f18eb4b99d847b1ba5caa23f6984731
ITVillage-Kevin <it.village.host@gmail.com>
2022-10-04 13:34:22 +0900

    Modify Example14_42, Example8_1

--- part2/src/main/java/chapter8/Example8_1.java
+++ part2/src/main/java/chapter8/Example8_1.java
@@ -1,5 +1,6 @@
 package chapter8;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Subscription;
 import reactor.core.publisher.BaseSubscriber;
@@ -13,26 +14,21 @@ import reactor.core.publisher.Flux;
 public class Example8_1 {
     public static void main(String[] args) {
         Flux.range(1, 5)
-            .doFinally(signalType -> log.info("# doFinally 1: {}", signalType))
-            .doFinally(signalType -> log.info("# doFinally 2: {}", signalType))
-            .doOnNext(data -> log.info("# range > doOnNext(): {}", data))
             .doOnRequest(data -> log.info("# doOnRequest: {}", data))
-            .doOnSubscribe(subscription -> log.info("# doOnSubscribe 1"))
-            .doFirst(() -> log.info("# doFirst()"))
-            .filter(num -> num % 2 == 1)
-            .doOnNext(data -> log.info("# filter > doOnNext(): {}", data))
-            .doOnComplete(() -> log.info("# doOnComplete()"))
-            .subscribe(new BaseSubscriber<>() {
+            .subscribe(new BaseSubscriber<Integer>() {
                 @Override
                 protected void hookOnSubscribe(Subscription subscription) {
                     request(1);
                 }
 
+                @SneakyThrows
                 @Override
                 protected void hookOnNext(Integer value) {
+                    Thread.sleep(2000L);
                     log.info("# hookOnNext: {}", value);
                     request(1);
                 }
             });
     }
 }
+
